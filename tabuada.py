8eef8e31b832c3b7fcb93d97dc50b6655c0473b7
lalsleo@gmail.com <lalsleo@gmail.com>
2022-08-24 11:45:25 -0300

    finalizando tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-"""Imprime a tabuada do 1 ao 10."""
+"""Imprime a tabuada do 1 ao 20."""
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Leo"
 
-#numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Iterable
 numeros = list(range(1, 11))
-carac = 18
+width = 18
 
 for n1 in numeros:
+    print("#" * width)
+    print()
     print("{:^18}".format(f"Tabuada do {n1}"))
     print()
     for n2 in numeros:
         resultado = n1 * n2
         print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
     print()
-    print("#" * 18)
+print("#" * width)
